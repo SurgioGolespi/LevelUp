351c0e4154fadcd1ab88e20f41b714904d4aff85
SurgioGolespi <ssamgajula@gmail.com>
2024-02-05 22:22:39 -0800

    Initial Commit

---
+++ LevelUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LevelUp
+//
+//  Created by Suraj Gajula on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LevelUp/LevelUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUpApp.swift
+//  LevelUp
+//
+//  Created by Suraj Gajula on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LevelUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
